b23bf709296f2628837fea2e28b289e04ac9df0e
Samin <bmst555m@gmail.com>
2022-03-02 22:09:42 +0330

    add binding to bout activity

--- app/src/main/java/com/example/tamrineighthsubmitinformation/ShowInfoActivity.kt
+++ app/src/main/java/com/example/tamrineighthsubmitinformation/ShowInfoActivity.kt
@@ -2,10 +2,13 @@ package com.example.tamrineighthsubmitinformation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tamrineighthsubmitinformation.databinding.ActivityShowInfoBinding
 
 class ShowInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityShowInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_info)
+        binding= ActivityShowInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
