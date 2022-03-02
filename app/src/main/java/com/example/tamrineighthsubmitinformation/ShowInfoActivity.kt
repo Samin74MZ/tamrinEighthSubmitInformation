334db8833aaa12bd94f336284eabb8dc21b3d1ab
Samin <bmst555m@gmail.com>
2022-03-02 22:06:22 +0330

    create layout file for bout activity

---
+++ app/src/main/java/com/example/tamrineighthsubmitinformation/ShowInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tamrineighthsubmitinformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_info)
+    }
+}
